c706271a3e05beee18556973371b9aa09064e2bd
Blaise Aranador <BlaiseAranador@gmail.com>
2022-02-22 11:22:47 +0000

    Added model to ping endpoint.

--- src/main/resources/api/endpoints/ping/ping.py
+++ src/main/resources/api/endpoints/ping/ping.py
@@ -7,7 +7,7 @@ api/endpoints/ping/ping.py:
 """
 
 
-from flask_restx import Namespace, Resource
+from flask_restx import Namespace, Resource, fields
 
 # Define ping endpoint namespace
 ping_endpoint = Namespace(
@@ -21,9 +21,26 @@ ping_endpoint = Namespace(
 )
 
 
+# Define ping endpoint model
+ping_endpoint_model = ping_endpoint.model(
+    "Ping",
+    {
+        "message": fields.String(
+            required=True, description="Message from ping endpoint."
+        ),
+        "status": fields.String(
+            required=True, description="Status from ping endpoint."
+        ),
+    },
+)
+
+
 # Define ping endpoint class
 class Ping(Resource):
     # Define ping endpoint GET
+    @ping_endpoint.marshal_with(ping_endpoint_model)
+    @ping_endpoint.response(200, "Successfully pinged endpoint.")
+    @ping_endpoint.response(400, "Failed to ping endpoint.")
     def get(self):
         return {"message": "pong", "status": "success"}
 
