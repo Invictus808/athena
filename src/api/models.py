a42645c18aa76471ccb6bcf12a54b72fbc4703aa
Blaise Aranador <BlaiseAranador@gmail.com>
2022-03-03 09:50:57 +0000

    Ran flake8, black, and isort Python modules.

--- src/api/models.py
+++ src/api/models.py
@@ -8,6 +8,7 @@ api/models.py:
 
 
 from sqlalchemy.sql import func
+
 from api import database
 
 
@@ -17,7 +18,9 @@ class User(database.Model):
     username = database.Column(database.String(128), nullable=False)
     email = database.Column(database.String(128), nullable=False)
     active = database.Column(database.String(128), default=True, nullable=False)
-    created_date = database.Column(database.DateTime, default=func.now(), nullable=False)
+    created_date = database.Column(
+        database.DateTime, default=func.now(), nullable=False
+    )
 
     def __init__(self, username, email):
         self.username = username
